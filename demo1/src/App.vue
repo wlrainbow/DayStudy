dd6901aa91b38188e9517ed936e4459c5f1b15c5
rainbow019 <726324902@qq.com>
2020-01-14 16:45:39 +0800

    新增iframe嵌套

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <iframe src="http://192.168.41.128:5011/#/creditCenter/foreignCompanyDetail/4409127?followId=325" frameborder="0" width="100%" height="1000px" allowFullScreen=true></iframe>
   </div>
 </template>
 
